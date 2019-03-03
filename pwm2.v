6c8ff93ab83f6ea009fb907ea4850e5f9c3d3613
Trammell hudson <hudson@trmm.net>
2019-03-03 12:28:37 -0500

    pwm module

--- pwm2.v
+++ pwm2.v
@@ -1,21 +1,14 @@
 module top(
-	output led_r,
-	output led_g,
-	output led_b
+	output led_r // defined in PCF file
 );
 	wire clk;
 	SB_HFOSC osc(1,1,clk); // 48 MHz oscillator
 
-	reg [18:0] counter; // 19-bit counter
-	reg [11:0] brightness = 10;
-	reg [11:0] max_brightness = 200; // out of 4096 (2^12)
+	reg [15:0] counter; // 16-bit counter
+	reg [11:0] brightness = 4000; // out of 4096 (2^12)
 	reg dir = 0;
 	
-	// continuous assignment to all three LEDs
-	wire led_on = counter[11:0] > brightness;
-	assign led_r = led_on;
-	assign led_g = led_on;
-	assign led_b = led_on;
+	pwm pwm_r(clk, brightness, led_r);
 
 	always @(posedge clk) begin
 		counter <= counter + 1;
@@ -23,7 +16,20 @@ module top(
 			brightness <= brightness + (dir ? 1 : -1);
 		if (brightness == 0)
 			dir <= 1; // hit bottom, go up
-		if (brightness == max_brightness)
+		if (brightness == 12'hFFF)
 			dir <= 0; // hit top, go down
 	end
+endmodule
+
+module pwm(
+	input clk,
+	input [WIDTH-1:0] brightness,
+	output led
+);
+	parameter WIDTH = 12;
+	reg [WIDTH-1:0] counter;
+	assign led = counter > brightness;
+
+	always @(posedge clk)
+		counter <= counter + 1;
 endmodule
